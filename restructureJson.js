b8cea4db0b849cbad46be73cd3569c59db9f850c
Eddy Leon <xltunited@gmail.com>
2024-02-05 05:57:49 +0100

    links by brand files

---
+++ restructureJson.js
@@ -0,0 +1,40 @@
+// Get links to traverse per brand
+import brands from './allBrandLinks.json' assert { type: "json" };
+
+// To write to file at the end
+import fs from 'fs';
+
+for( let brand of brands) {
+
+    let models = brand.models;
+
+    let fileName = brand.brandName;
+
+    let htmlsToDownload = [];
+
+    for(let model of models) {
+
+        for(let version of model){
+
+            if(("versions" in version)){
+
+                for(let variants of version.versions) {
+
+                    htmlsToDownload.push(variants.versionLink)
+
+                }
+
+            }
+
+        }
+
+    }
+
+    let jsonArray = JSON.stringify(htmlsToDownload);
+
+    fs.writeFile('./brandVariants/'+fileName+'.json', jsonArray, 'utf8', function(err) {
+        if (err) throw err;
+        console.log('complete');
+    });
+
+}
